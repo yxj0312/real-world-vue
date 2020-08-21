490ff54b6f07b2c7f6b1e8639507ead0fbfd7ff1
yxj0312 <yanxiajun@hotmail.com>
2020-08-21 23:37:34 +0200

    Add login view

--- src/router/index.js
+++ src/router/index.js
@@ -8,18 +8,19 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
       name: "home",
+      path: "/",
       component: () => import("@/views/Home")
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "@/views/About.vue")
+      name: "login",
+      path: "/login",
+      component: () => import("@/views/Login")
+    },
+    {
+      name: "register",
+      path: "/register",
+      component: () => import("@/views/Register")
     }
   ]
 });
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign in</h1>
+          <p class="text-xs-center">
+            <router-link :to="{ name: 'register' }">
+              Need an account?
+            </router-link>
+          </p>
+          <ul v-if="errors" class="error-messages">
+            <li v-for="(v, k) in errors" :key="k">{{ k }} {{ v | error }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "RwvLogin",
+
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    })
+  }
+};
+</script>
