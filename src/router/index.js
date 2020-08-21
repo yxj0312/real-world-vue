c9e5407587525070c73f5bc0a82013201cbb1059
yxj0312 <yanxiajun@hotmail.com>
2020-08-21 14:51:04 +0200

    Move router.js to router folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -11,7 +10,7 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () => import("@/views/Home")
     },
     {
       path: "/about",
@@ -20,7 +19,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ "@/views/About.vue")
     }
   ]
 });
