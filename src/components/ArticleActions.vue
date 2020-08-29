db0d157ae4d4392e310059050ae8208d98d9a194
xiajunyan <yanxiajun@hotmail.com>
2020-08-29 21:32:31 +0200

    Add template to ArticleActions.vue, and test vue on macbook

---
+++ src/components/ArticleActions.vue
@@ -0,0 +1,7 @@
+<template>
+  <span v-if="canModify">
+    <router-link class="btn btn-sm btn-outline-secondary" :to="editArticleLink">
+      <i class="ion-edit"></i><span>&nbsp;Edit Article</span>
+    </router-link>
+  </span>
+</template>
