0ab1d2b4160c0c72d4cf3057f9c774e6421d4828
xiajunyan <yanxiajun@hotmail.com>
2020-08-30 20:22:48 +0200

    Add template and script to ArticleActions.vue

--- src/components/ArticleActions.vue
+++ src/components/ArticleActions.vue
@@ -3,5 +3,41 @@
     <router-link class="btn btn-sm btn-outline-secondary" :to="editArticleLink">
       <i class="ion-edit"></i><span>&nbsp;Edit Article</span>
     </router-link>
+    <span>&nbsp;&nbsp;</span>
+    <button class="btn btn-outline-danger btn-sm" @click="deleteArticle">
+      <i class="ion-trash-a"></i><span>&nbsp;Delete Article</span>
+    </button>
+  </span>
+  <span v-else>
+    <button class="btn btn-sm btn-outline-secondary" @click="toggleFollow">
+      <i class="ion-plus-round"></i> <span>&nbsp;</span>
+      <span v-text="followUserLabel"></span>
+    </button>
+    <span>&nbsp;&nbsp;</span>
+    <button
+      class="btn btn-sm"
+      @click="toggleFavorite"
+      :class="toggleFavoriteButtonClasses"
+    >
+      <i class="ion-heart"></i> <span>&nbsp;</span>
+      <span v-text="favoriteArticleLabel" /> <span>&nbsp;</span>
+      <span class="counter" v-text="favoriteCounter" />
+    </button>
   </span>
 </template>
+
+<script>
+export default {
+  name: "RwvArticleActions",
+  props: {
+    article: {
+      type: Object,
+      required: true
+    },
+    canModify: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
