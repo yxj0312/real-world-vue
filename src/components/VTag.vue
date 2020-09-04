869b1ec5b5ba8ed68831d1392f8c2bb49a2bcecd
yxj0312 <yanxiajun@hotmail.com>
2020-09-04 22:21:08 +0200

    Add VTag.vue

---
+++ src/components/VTag.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-link :to="homeRoute" :class="className"></router-link>
+</template>
+
+<script>
+export default {
+  name: "RwvTag",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: "tag-pill tag-default"
+    }
+  },
+  computed: {
+    homeRoute: () => ({ name: "home-tag", param: { tag: name } })
+  }
+};
+</script>
