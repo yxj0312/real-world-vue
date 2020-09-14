b67f2d60d67b9772598ed200bc93daf379696094
yxj0312 <yanxiajun@hotmail.com>
2020-09-14 22:58:14 +0200

    Add VTag to Article.vue

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -9,6 +9,14 @@
         <div class="row article-content">
           <div class="col-xs-12">
             <div v-html="parseMarkdown(article.body)"></div>
+            <ul class="tag-list">
+              <li v-for="(tag, index) of article.tagList" :key="tag + index">
+                <RwvTag
+                  :name="tag"
+                  className="tag-default tag-pill tag-outline"
+                />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -17,6 +25,7 @@
 </template>
 
 <script>
+import RwvTag from "@/components/VTag";
 import { mapGetters } from "vuex";
 import marked from "marked";
 import store from "@/store";
@@ -31,7 +40,7 @@ export default {
       required: true
     }
   },
-  components: { RwvArticleMeta },
+  components: { RwvTag, RwvArticleMeta },
   beforeRouteEnter(to, from, next) {
     Promise.all([store.dispatch(FETCH_ARTICLE, to.params.slug)]).then(() => {
       next();
