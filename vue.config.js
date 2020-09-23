a09bc13eea142ac344604e0326b0eb58a658f66d
yxj0312 <yanxiajun@hotmail.com>
2020-09-24 00:03:33 +0200

    Add deploy.sh and vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js
 module.exports = {
-  devServer: {
-    proxy: "https://conduit.productionready.io/api"
-  }
+  // devServer: {
+  //   proxy: "https://conduit.productionready.io/api"
+  // }
+  // publicPath: "/"
 };
